8c06b6a71c4bd4c6430bef4bbcbdfa3c59b3e1ca
Ryan Pecor <rpecor14@gmail.com>
2018-01-27 23:29:37 -0500

    Tinkering with vue

--- client/src/components/About/Team.vue
+++ client/src/components/About/Team.vue
@@ -1,20 +1,22 @@
 <template>
-<v-layout>
-  <v-flex xs7>
+<v-layout center>
+  <v-flex transition="slide-x-transition" xs6 offset-xs1 >
   <div class="hello">
     
-    <h3 >Ryan Pecor</h3>
-    <h3> I love travel. </h3>
-    
+    <h3 class="name">Ryan Pecor</h3>
+    <h4 class="name"> I'm a guy who believes in the power of travel and the impact it has on people.</h4>
+    <br/>
+    <br/>
+    <br/>
+    <h5 class="name">Always in need of motivated designers, developers, marketers and product people! Reach out if you'd like to help.</h5>
   </div>
   </v-flex>
   
-  <v-flex xs5>
+  <v-flex xs4>
   <div class="white elevation-2 ">
-      <div class="pt-2 pl-4 pr-4 pb-2">
-        <slot>
-        No slot content
-        </slot>
+      <div class="pt-4 pl-4 pr-4 pb-4">
+        <img class="image" :src="image" />
+        
       </div>
     </div>
   </v-flex>
@@ -23,10 +25,22 @@
 </template>
 
 <script>
+
 export default {
+  data () {
+    return {
+      image: require('../../assets/me2.jpg')
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.image {
+  max-width: 100%;
+}
+.name {
+  text-align: left ;
+}
 </style>
